5df7bfae5649ad6ee212a8a2579dd02d6be80162
Avni.rawal <Avni1.ecafe@gmail.com>
2024-10-05 03:17:59 +0000

    Implemented Oath login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,18 @@
+// routes/auth.js
 const express = require('express');
-const { registerUser, loginUser, verifyEmail } = require('../controllers/authController');
+const passport = require('passport');
+const { registerUser, loginUser, verifyEmail, googleCallback } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
 router.get('/verify/:token', verifyEmail);
+
+// Google OAuth routes
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), googleCallback);
 
 module.exports = router;
